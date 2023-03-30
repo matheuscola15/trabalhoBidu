db4221fc3ad8bc6bc09f00853942aefad0229a93
matheuscola15 <matheuscola295@gmail.com>
2023-03-30 08:57:47 -0300

    Initial commit

---
+++ src/main/java/br/com/matheus/trabalhoBidu/TrabalhoBiduApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheus.trabalhoBidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoBiduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoBiduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matheus/trabalhoBidu/TrabalhoBiduApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheus.trabalhoBidu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoBiduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
